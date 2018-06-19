59eecb4fbccffc563ba8c21f39200771a787489b
rullocarullo <cicciopio>
2018-06-19 11:40:38 +0200

    nuovaFeature1

---
+++ corsoDevOpsVS/corsoDevOpsVS/ClassNando.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace corsoDevOpsVS
+{
+    public class ClassNando
+    {
+        public int PrimaProprieta { get; set; }
+        public int SecondaProprieta { get; set; }
+    }
+}
