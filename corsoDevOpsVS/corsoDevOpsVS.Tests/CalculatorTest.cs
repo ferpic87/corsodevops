e6d548e8ca2e16d62537a7e7ffdc963d5790c063
bartolomeo <b.ovilio@gmail.com>
2018-06-20 09:41:36 +0200

    calculator

---
+++ corsoDevOpsVS/corsoDevOpsVS.Tests/CalculatorTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace corsoDevOpsVS.Tests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            //Arrangiati
+            int a = 5;
+            int b = 4;
+            int expected = 9;
+
+            //Activati
+            int actual = new Calculator().Add(a, b);
+
+            //Assertati
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            //Arrangiati
+            int a = 5;
+            int b = 4;
+            int expected = 1;
+
+            //Activati
+            int actual = new Calculator().Subtract(a, b);
+
+            //Assertati
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            //Arrangiati
+            int a = 5;
+            int b = 4;
+            int expected = 20;
+
+            //Activati
+            int actual = new Calculator().Multiply(a, b);
+
+            //Assertati
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            //Arrangiati
+            int a = 5;
+            int b = 4;
+            int expected = 1;
+
+            //Activati
+            int actual = new Calculator().Divide(a, b);
+
+            //Assertati
+            Assert.AreEqual(expected, actual);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void DivideByZeroTest()
+        {
+            //Arrangiati
+            int a = 5;
+            int b = 0;
+            int expected = 1;
+
+            //Activati
+            int actual = new Calculator().Divide(a, b);
+
+            //Assertati
+            //Assert.AreEqual(expected, actual);
+        }
+    }
+
+
+}
---
+++ corsoDevOpsVS/corsoDevOpsVS.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("corsoDevOpsVS.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("corsoDevOpsVS.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0cbfc0d-f450-47b7-b15f-c55a63b40915")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
